83577ed23c6a24ec02a2d33b1b462dda196d214c
kriti-sc <38242933+kriti-sc@users.noreply.github.com>
2020-05-12 01:01:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+import re
+
+packages = find_packages()
+
+reqs = []
+with open("requirements.txt") as f:
+    for line in f:
+        if line.startswith('git+https'):
+            continue
+        reqs.append(line.strip())
+
 
 __version__ = '0.0.1'
 
 setup_args = dict(
     name='atlan_lite',
     version='0.0.1',
-    description='Plugin to push airflow lineage to Atlan'
+    description='Plugin to push airflow lineage to Atlan',
+    packages=packages,
+    include_package_data=True,
+    install_requires=reqs
 )
 
 
