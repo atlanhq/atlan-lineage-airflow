3fc00313375247c09b3e4990da79220f593b7e4d
Kriti Kathuria <kriti@atlan.com>
2020-05-31 11:06:47 +0000

    expose API as airflow plugin

--- setup.py
+++ setup.py
@@ -57,7 +57,12 @@ setup_args = dict(
     license='Apache License 2.0',
     packages=packages,
     include_package_data=True,
-    install_requires=reqs
+    install_requires=reqs,
+    entry_points={
+        'airflow.plugins': [
+            'atlan.api = atlan.api:AtlanRESTApiPlugin'
+        ]
+    }
 )
 
 if __name__ == '__main__':
